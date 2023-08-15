fdd45250243d7e70c11e7cfedb8cc89b86220fe3
elgharbi <elgharbiay@gmail.com>
2023-08-16 00:35:29 +0100

    add tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+""" Tests for class BaseModel """
 import unittest
 from models.base_model import BaseModel
 from datetime import datetime
@@ -5,13 +7,17 @@ import models
 
 
 class TestBaseModel(unittest.TestCase):
+    """ Tests for class BaseModel """
+
     def test_init(self):
+        """Tests for init """
         model = BaseModel()
         self.assertTrue(hasattr(model, "id"))
         self.assertTrue(hasattr(model, "created_at"))
         self.assertTrue(hasattr(model, "updated_at"))
 
     def test_str(self):
+        """Test __str__ method"""
         model = BaseModel()
         str_repr = str(model)
         self.assertIn("[BaseModel]", str_repr)
@@ -20,12 +26,14 @@ class TestBaseModel(unittest.TestCase):
         self.assertIn("updated_at", str_repr)
 
     def test_save(self):
+        """Test save method"""
         model = BaseModel()
         old_updated_at = model.updated_at
         model.save()
         self.assertNotEqual(old_updated_at, model.updated_at)
 
     def test_to_dict(self):
+        """Test to_dict method"""
         model = BaseModel()
         model_dict = model.to_dict()
         self.assertEqual(model_dict['__class__'], "BaseModel")
@@ -49,6 +57,7 @@ class TestBaseModel(unittest.TestCase):
 
 
     def test_save_existing_instance(self):
+        """Test saving a existing instance from dictionary"""
         model = BaseModel()
         old_updated_at = model.updated_at
         model.save()
---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,43 @@
+import unittest
+import os
+import json
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+
+
+class TestFileStorage(unittest.TestCase):
+
+    def setUp(self):
+        self.storage = FileStorage()
+
+    def tearDown(self):
+        try:
+            os.remove(FileStorage._FileStorage__file_path)
+        except FileNotFoundError:
+            pass
+
+    def test_all(self):
+        """Test the 'all' method of FileStorage"""
+        pass
+
+    def test_new(self):
+        """Test the 'new' method of FileStorage"""
+        model = BaseModel()
+        self.storage.new(model)
+        all_objects = self.storage.all()
+        self.assertIn('BaseModel.{}'.format(model.id), all_objects)
+
+    def test_save_reload(self):
+        """Test saving and reloading using FileStorage"""
+        model = BaseModel()
+        self.storage.new(model)
+        self.storage.save()
+        self.storage = None
+        new_storage = FileStorage()
+        new_storage.reload()
+        all_objects = new_storage.all()
+        self.assertIn('BaseModel.{}'.format(model.id), all_objects)
+
+
+if __name__ == '__main__':
+    unittest.main()
